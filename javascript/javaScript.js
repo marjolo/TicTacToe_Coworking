aa14f83f70b431a8ef3e7bb1724833878bae53a0
Jarno <jarno.vanosselaer@student.odisee.be>
2022-04-29 10:24:43 +0200

    Eerste java en css test

---
+++ javascript/javaScript.js
@@ -0,0 +1,21 @@
+let board;
+
+window.onload = function (){
+    setBoard();
+}
+
+function setBoard() {
+
+    board = [
+        [' ', ' ', ' '],
+        [' ', ' ', ' '],
+        [' ', ' ', ' ']
+    ]
+
+    for (let r = 0; r < 3; r++){
+        for (let k = 0; k < 3; k++){
+            let tile = document.createElement("div");
+        }
+    }
+
+}
