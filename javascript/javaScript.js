8182b9242d5b7f584c52e37bc7317dfa98e4d588
Jarno <jarno.vanosselaer@student.odisee.be>
2022-04-29 11:13:48 +0200

    Board getekend via javasript

--- javascript/javaScript.js
+++ javascript/javaScript.js
@@ -1,6 +1,7 @@
 let board;
 
 window.onload = function (){
+    console.log("Window loaded")
     setBoard();
 }
 
@@ -14,8 +15,21 @@ function setBoard() {
 
     for (let r = 0; r < 3; r++){
         for (let k = 0; k < 3; k++){
+            //<div id="0-0" class="tile"></div>
             let tile = document.createElement("div");
+            tile.id = r.toString() + "-" + k.toString();
+            tile.classList.add("tile");
+
+            if (r === 0 || r === 1) {
+                tile.classList.add("horizontal-line");
+            }
+            if (k === 0 || k === 1) {
+                tile.classList.add("vertical-line");
+            }
+
+            document.getElementById("board").append(tile);
         }
     }
 
+    console.log("Board set")
 }
